3e0e6a34bf0aa63b64e726d9079585d4b46fed64
mdrayhn3331-art <mdrayhn3331@gmail.com>
2026-08-07 06:28:01 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,92 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Products from "./pages/Products";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-export default function App() {
-  return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
 
-      {/* Protected Route */}
+export default function App() {
+
+  return (
+
+    <Routes>
+
+
+      {/* Public Routes */}
+
       <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
+        path="/login"
+        element={<Login />}
       />
 
-      {/* 404 */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+
+
+      {/* Protected Routes */}
+
+      <Route
+
+        path="/"
+
+        element={
+
+          <ProtectedRoute>
+
+            <Home />
+
+          </ProtectedRoute>
+
+        }
+
+      />
+
+
+      <Route
+
+        path="/products"
+
+        element={
+
+          <ProtectedRoute>
+
+            <Products />
+
+          </ProtectedRoute>
+
+        }
+
+      />
+
+
+
+      {/* 404 Redirect */}
+
+      <Route
+
+        path="*"
+
+        element={
+
+          <Navigate
+
+            to="/"
+
+            replace
+
+          />
+
+        }
+
+      />
+
+
     </Routes>
+
   );
+
 }
